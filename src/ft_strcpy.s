eb4ccf849c1304088bc1ba4013bce99f9bc76722
Timoti Fregni <timotif@gmail.com>
2024-10-10 10:29:34 +0200

    better version: 42% faster on a 4096-bytes string

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -2,23 +2,36 @@ section .text
 	global ft_strcpy
 	extern ft_strlen
 
-ft_strcpy:
-	mov rbx, rdi ; save to rbx the first arg: dst
-	mov rdi, rsi ; move to rdi the src so that it's the arg for ft_strlen
-	call ft_strlen ; call ft_strlen on the src
-	; rax contains the length of the src
-	; rdi contains the dst
-	; rsi contains the src
+; First approach
+; ft_strcpy:
+; 	mov rbx, rdi ; save to rbx the first arg: dst
+; 	mov rdi, rsi ; move to rdi the src so that it's the arg for ft_strlen
+; 	call ft_strlen ; call ft_strlen on the src
+; 	; rax contains the length of the src
+; 	; rdi contains the dst
+; 	; rsi contains the src
+; 
+; _loop:
+;     mov cl, [rsi + rax]  ; load the byte from src into cl
+;     mov [rbx + rax], cl  ; store the byte into dst
+; 	cmp rax, 0 ; compare to 0
+; 	je _return
+; 	dec rax ; decrease after the check to avoid rax to ever go negative (invalid read)
+; 	jmp _loop
+; _return:
+; 	mov rax, rbx ; return the dst
+; 	ret
 
-_loop:
-    mov cl, [rsi + rax]  ; load the byte from src into cl
-    mov [rbx + rax], cl  ; store the byte into dst
-	cmp rax, 0 ; compare to 0
-	je _return
-	dec rax ; decrease after the check to avoid rax to ever go negative (invalid read)
-	jmp _loop
-_return:
-	mov rax, rbx ; return the dst
+; Better approach using movsb (42% improvement on a 4MB-long string)
+ft_strcpy:
+	mov rbx, rdi ; save away rdi
+	mov rdi, rsi ; set source string as arg for strlen
+	call ft_strlen
+	mov rdi, rbx ; get back rdi original content
+	mov rcx, rax ; set the counter for rep on the return value of strlen
+	inc rcx ; include null terminator
+	rep movsb ; move a byte from rsi to rdi rcx times
+	mov rax, rbx ; set return value to the original dst value
 	ret
 
 section .note.GNU-stack
