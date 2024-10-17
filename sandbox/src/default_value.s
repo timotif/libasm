b61147f6cdea7bb25acb155d20fefde10fe84bc6
Timoti Fregni <timotif@gmail.com>
2024-10-17 22:41:04 +0200

    moved sandbox folder to sandbox branch

--- sandbox/src/default_value.s
+++
@@ -1,9 +0,0 @@
-section .data
-	num db 3 dup(2) ; 3 bytes, each with value 2
-section .text
-	global _start
-
-_start:
-	mov ebx, [num]
-	mov eax, 1
-	int 80h
--- sandbox/src/first.s
+++
@@ -1,8 +0,0 @@
-section .data
-section .text
-	global _start
-
-_start:
-	mov eax, 1 ; what system call
-	mov ebx, 42 ; status code
-	int 80h ; h for hex
